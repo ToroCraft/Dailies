9dd79d98ee2c6fb03be4e4d0c827e4b3b94b36d2
Zane Riley <zeriley@gmail.com>
2016-08-20 11:26:56 -0400

    overriding DisplayName to display Bailey

--- src/main/java/net/torocraft/dailies/entities/EntityBailey.java
+++ src/main/java/net/torocraft/dailies/entities/EntityBailey.java
@@ -2,26 +2,26 @@ package net.torocraft.dailies.entities;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.scoreboard.ScorePlayerTeam;
+import net.minecraft.scoreboard.Team;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.village.MerchantRecipeList;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.torocraft.dailies.DailiesGuiHandler;
 import net.torocraft.dailies.DailiesMod;
-import net.torocraft.dailies.entities.render.RenderBailey;
 
 public class EntityBailey extends EntityVillager {
 
 	private MerchantRecipeList buyingList;
-	
+
 	public static String NAME = "bailey";
 
 	public EntityBailey(World world) {
@@ -38,7 +38,8 @@ public class EntityBailey extends EntityVillager {
 
 		if (!flag && this.isEntityAlive() && !this.isTrading() && !this.isChild() && !player.isSneaking()) {
 			if (!this.worldObj.isRemote && (this.buyingList == null || !this.buyingList.isEmpty())) {
-				player.openGui(DailiesMod.instance, DailiesGuiHandler.getGuiID(), this.worldObj, player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
+				player.openGui(DailiesMod.instance, DailiesGuiHandler.getGuiID(), this.worldObj,
+						player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
 			}
 
 			player.addStat(StatList.TALKED_TO_VILLAGER);
@@ -46,5 +47,20 @@ public class EntityBailey extends EntityVillager {
 		} else {
 			return super.processInteract(player, hand, stack);
 		}
+	}
+
+	@Override
+	public ITextComponent getDisplayName() {
+		Team team = this.getTeam();
+		String name = this.getCustomNameTag();
+
+		if (name == null || name.length() == 0) {
+			name = "Bailey";
+		}
+
+		TextComponentString textcomponentstring = new TextComponentString(ScorePlayerTeam.formatPlayerName(team, name));
+		textcomponentstring.getStyle().setHoverEvent(this.getHoverEvent());
+		textcomponentstring.getStyle().setInsertion(this.getCachedUniqueIdString());
+		return textcomponentstring;
 	}
 }
