a2ca2ccc9e539e5f2bba02c1f3816a6b2b736f0d
Paul Anderson <thesamuraipanda@gmail.com>
2016-09-04 08:16:30 -0400

    versioning to 1.9.4-12

--- src/main/java/net/torocraft/dailies/DailiesMod.java
+++ src/main/java/net/torocraft/dailies/DailiesMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class DailiesMod {
 
 	public static final String MODID = "dailies";
-	public static final String VERSION = "1.9.4-10";
+	public static final String VERSION = "1.9.4-12";
 	public static final String MODNAME = "DailiesMod";
 	public static final Integer MAX_QUESTS_ACCEPTABLE = 10;
 
