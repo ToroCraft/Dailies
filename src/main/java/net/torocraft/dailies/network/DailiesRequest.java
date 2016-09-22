8ba81c2b581f40130d017b600313998651387271
Paul Anderson <thesamuraipanda@gmail.com>
2016-09-22 07:08:48 -0400

    include accessToken in requests for authentication #25 resolved

--- src/main/java/net/torocraft/dailies/network/DailiesRequest.java
+++ src/main/java/net/torocraft/dailies/network/DailiesRequest.java
@@ -3,14 +3,18 @@ package net.torocraft.dailies.network;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import net.minecraft.client.Minecraft;
 import net.torocraft.dailies.DailiesMod;
 
 public class DailiesRequest {
 	
 	public String modVersion;
+	public String accessToken;
 	
 	public DailiesRequest() {
 		modVersion = DailiesMod.metadata.version;
+		accessToken = Minecraft.getMinecraft().getSession().getToken();
+		System.out.println("accessToken = " + accessToken);
 	}
 	
 	public String serialize() {
