691b0141eee031eb309870091f90af317fa425fd
Paul Anderson <thesamuraipanda@gmail.com>
2016-09-22 07:21:40 -0400

    include minecraft user uuid in requests. resolves #35

--- src/main/java/net/torocraft/dailies/network/DailiesRequest.java
+++ src/main/java/net/torocraft/dailies/network/DailiesRequest.java
@@ -4,16 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.Session;
 import net.torocraft.dailies.DailiesMod;
 
 public class DailiesRequest {
 	
 	public String modVersion;
 	public String accessToken;
+	public String playerId;
 	
 	public DailiesRequest() {
 		modVersion = DailiesMod.metadata.version;
-		accessToken = Minecraft.getMinecraft().getSession().getToken();
+		Session session = Minecraft.getMinecraft().getSession();
+		accessToken = session.getToken();
+		playerId = session.getPlayerID();
+		System.out.println("playerId = " + playerId);
 	}
 	
 	public String serialize() {
